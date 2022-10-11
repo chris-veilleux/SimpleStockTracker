fab270930f980c09a9065474f6c70e4f54905078
chris-veilleux <82720132+chris-veilleux@users.noreply.github.com>
2022-10-11 15:31:26 -0400

    Add models, views, and controllers to project with basic functionality

---
+++ SimpleStockTracker/Controllers/OverviewController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SimpleStockTracker.Models;
+
+namespace SimpleStockTracker.Controllers
+{
+    public class OverviewController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Account(string Nickname)
+        {
+            // if no parameter is provided, redirect the user to the list of nicknames
+            if (Nickname == null)
+            {
+                return RedirectToAction("Index");
+            }
+
+            // pass the input parameter for the account nickname to the ViewData object
+            ViewData["Nickname"] = Nickname;
+
+            // hardcode a list of holdings to display for now
+            var holdings = new List<Holding>();
+            for (var i = 0; i < 11; i++)
+            {
+                holdings.Add(new Holding { HoldingId = i, Ticker = "Ticker #" + i.ToString(), TradeDate = DateTime.Now, TradeType="Buy", Quantity=2, Price=3.21m, Account="RESP" });
+            }
+
+            return View(holdings);
+        }
+    }
+}
---
+++ SimpleStockTracker/Models/Holding.cs
@@ -0,0 +1,13 @@
+﻿namespace SimpleStockTracker.Models
+{
+    public class Holding
+    {
+        public int HoldingId { get; set; }
+        public string? Ticker { get; set; }
+        public DateTime TradeDate { get; set; }
+        public string? TradeType { get; set; }
+        public int Quantity { get; set; }
+        public decimal Price { get; set; }
+        public string? Account { get; set; }
+    }
+}
