5ef74ad9dcd3a6933f1fb1e7ff9a31b8bbee2576
chris-veilleux <82720132+chris-veilleux@users.noreply.github.com>
2022-11-14 16:11:06 -0500

    Add Google authentication

--- SimpleStockTracker/Program.cs
+++ SimpleStockTracker/Program.cs
@@ -14,6 +14,14 @@ builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.Requ
     .AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddControllersWithViews();
 
+// Google auth - keys stored in appsettings.json
+builder.Services.AddAuthentication()
+    .AddGoogle(options =>
+    {
+        options.ClientId = builder.Configuration["Authentication:Google:ClientId"];
+        options.ClientSecret = builder.Configuration["Authentication:Google:ClientSecret"];
+    });
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
