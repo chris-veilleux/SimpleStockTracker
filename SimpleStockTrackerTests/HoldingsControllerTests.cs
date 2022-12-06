8f2df7fb7886c59b2bc69d6a2fcc737e7b56ee53
chris-veilleux <82720132+chris-veilleux@users.noreply.github.com>
2022-12-06 18:22:53 -0500

    Add tests project and set up files

---
+++ SimpleStockTrackerTests/HoldingsControllerTests.cs
@@ -0,0 +1,11 @@
+namespace SimpleStockTrackerTests
+{
+    [TestClass]
+    public class HoldingsControllerTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
